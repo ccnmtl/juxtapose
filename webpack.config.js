c434779d78563545307f5af1774fc3e8cbc8531f
Nik Nyby <nnyby@columbia.edu>
2016-06-28 10:13:52 -0400

    use babel-preset-es2015

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,12 @@ var config = {
     module: {
         loaders: [
             {
-                test: /\.jsx?/,
+                test: /\.jsx?$/,
                 include: APP_DIR,
-                loader: 'babel'
+                loader: 'babel',
+		        query: {
+			        presets: ['react', 'es2015']
+		        }
             }
         ]
     }
