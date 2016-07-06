f697c090572a6a4996b52455c240028e55e4de96
Nik Nyby <nnyby@columbia.edu>
2016-07-06 16:10:30 -0400

    more organization

---
+++ src/JuxtaposeApplication.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import ReactGridLayout from 'react-grid-layout';
+import {pad2, formatDuration} from './utils.js';
+import {auxTrackData, textTrackData} from './data.js';
+import {AuxTrack, AuxMedia} from './aux.jsx';
+import {TextTrack, TextDisplay} from './text.jsx';
+import PlayButton from './PlayButton.jsx';
+import SpineVideo from './SpineVideo.jsx';
+
+
+class SpineTrack extends React.Component {
+    render() {
+        return <div className="jux-spine-track">Spine Track</div>;
+    }
+}
+
+class MediaPopup extends React.Component {
+    render() {
+        return <div className="jux-popup"
+                    ref={(ref) => this.el = ref}
+                    onClick={this.handlePopupClick.bind(this)}>
+            Mediathread Collection
+            <button className="jux-close"
+                    onClick={this.closePopup.bind(this)}>X</button>
+        </div>;
+    }
+    handlePopupClick(event) {
+        event.stopPropagation();
+    }
+    openPopup(event) {
+        var x = event.clientX - this.el.offsetLeft;
+        this.el.style.left = x + 'px';
+        this.el.style.visibility = 'visible';
+    }
+    closePopup(event) {
+        event.stopPropagation();
+        this.el.style.visibility = 'hidden';
+    }
+}
+
+class Playhead extends React.Component {
+    constructor() {
+        super();
+        this.state = {time: 0, duration: null};
+    }
+    handleChange(event) {
+        var percentDone = event.target.value / 1000;
+        var newTime = this.state.duration * percentDone;
+        this.setState({
+            time: newTime,
+            duration: this.state.duration
+        });
+        this.props.callbackParent(newTime, this.state.duration);
+    }
+    render() {
+        var currentPos = 0;
+        if (this.state.duration !== 0) {
+            currentPos = (this.state.time / this.state.duration);
+        }
+
+        var clientWidth = this.el ? this.el.clientWidth : 0;
+        var offsetLeft = this.el ? this.el.offsetLeft : 0;
+        var x = clientWidth * currentPos + offsetLeft;
+        var lineStyle = {left: x + 'px'};
+
+        return <div>
+            <div
+                ref={(ref) => this._line = ref}
+                className="jux-playhead-line"
+                style={lineStyle}>
+                <div className="jux-playhead-top-cutpoint"></div>
+                <div className="jux-playhead-bottom-cutpoint"></div>
+            </div>
+            <input type="range" min="0" max="1000"
+                   ref={(ref) => this.el = ref}
+                   onChange={this.handleChange.bind(this)}
+                   value={currentPos * 1000} />
+        </div>;
+    }
+}
+
+export default class JuxtaposeApplication extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            time: null,
+            duration: null,
+            auxTrack: auxTrackData,
+            textTrack: textTrackData
+        };
+    }
+    render() {
+        return <div className="jux-container">
+            <div className="vid-container">
+                <SpineVideo
+                    ref={(c) => this._spineVid = c}
+                    callbackParent={this.onTimeUpdate.bind(this)} />
+                <AuxMedia ref={(c) => this._auxVid = c} />
+            </div>
+            <TextDisplay time={this.state.time}
+                         duration={this.state.duration} />
+            <PlayButton callbackParent={this.onPlayChanged.bind(this)} />
+            <div className="jux-time">
+                {formatDuration(this.state.time)} / {formatDuration(this.state.duration)}
+            </div>
+            <div className="jux-timeline">
+                <Playhead ref={(c) => this._playhead = c}
+                          callbackParent={this.onPlayheadUpdate.bind(this)} />
+                <SpineTrack />
+                <AuxTrack duration={this.state.duration}
+                          data={this.state.auxTrack} />
+                <TextTrack duration={this.state.duration}
+                           data={this.state.textTrack} />
+            </div>
+        </div>;
+    }
+    onPlayChanged(play) {
+        if (play) {
+            this._spineVid.play();
+            this._auxVid.play();
+        } else {
+            this._spineVid.pause();
+            this._auxVid.pause();
+        }
+    }
+    onTimeUpdate(time, duration) {
+        // TODO: this works for now, but I have a feeling this isn't the
+        // right way to share state. Even if it is... the code is messy.
+        var state = {
+            time: time,
+            duration: duration
+        };
+        this._playhead.setState(state);
+        this.setState(state);
+    }
+    onPlayheadUpdate(time, duration) {
+        var state = {
+            time: time,
+            duration: duration
+        };
+        this._spineVid.setState(state);
+        this._spineVid.updateVidPosition(time);
+        this.setState(state);
+    }
+}
---
+++ src/SpineVideo.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class SpineVideo extends React.Component {
+    constructor() {
+        super();
+        this.id = 'jux-spine-video';
+        this.state = {duration: null, time: null};
+    }
+    render() {
+        return <div>
+            <video id={this.id}
+                   ref={(ref) => this.el = ref}
+                   width="297"
+                   onTimeUpdate={this.handleTimeUpdate.bind(this)}
+                   onLoadedMetadata={this.handleLoadedMetadata.bind(this)}>
+                <source src="wildspot.mp4" type="video/mp4" />
+                <source src="wildspot.ogv"
+                        type='video/ogg; codecs="theora, vorbis"' />
+            </video>
+        </div>;
+    }
+    updateVidPosition(time) {
+        this.el.currentTime = time;
+    }
+    handleLoadedMetadata(e) {
+        var vid = e.target;
+        this.setState({time: vid.currentTime, duration: vid.duration});
+        this.props.callbackParent(vid.currentTime, vid.duration);
+    }
+    handleTimeUpdate(e) {
+        var vid = e.target;
+        this.setState({time: vid.currentTime, duration: this.state.duration});
+        this.props.callbackParent(vid.currentTime, this.state.duration);
+    }
+    // TODO: handle playback finish event
+    play() {
+        var vid = this.el;
+        this.setState({time: vid.currentTime, duration: vid.duration});
+        vid.play();
+    }
+    pause() {
+        this.el.pause();
+    }
+}
