e50dc94c57d3dc53cd907b21ed60848e1d5668a0
Nik Nyby <nnyby@columbia.edu>
2016-07-11 11:16:03 -0400

    Connect vid/img track to dynamic display

--- src/aux.jsx
+++ src/aux.jsx
@@ -1,29 +1,50 @@
 import React from 'react';
 import Track from './Track.jsx';
+import {auxTrackData} from './data.js';
 
 export class AuxTrack extends Track {
 }
 
-export class AuxMedia extends React.Component {
+export class AuxDisplay extends React.Component {
     constructor() {
         super()
         this.id = 'jux-aux-video';
         this.state = {};
     }
+    renderVideo(data) {
+        return <video id={this.id}
+                      ref={(ref) => this.el = ref}
+                      width="297">
+            <source src="wildspot.mp4" type="video/mp4" />
+            <source src="wildspot.ogv"
+                    type='video/ogg; codecs="theora, vorbis"' />
+        </video>;
+    }
+    renderImage(data) {
+        return <img src={data.source} />;
+    }
+    nowDisplay(data, currentTime) {
+        for (let e of data) {
+            if (currentTime >= e.startTime &&
+                currentTime <= e.endTime) {
+                if (e.type === 'vid') {
+                    return this.renderVideo(e);
+                } else {
+                    return this.renderImage(e);
+                }
+            }
+        }
+        return '';
+    }
     render() {
-        return <div>
-            <video id={this.id}
-                   ref={(ref) => this.el = ref}
-                   width="297">
-                <source src="wildspot.mp4" type="video/mp4" />
-                <source src="wildspot.ogv"
-                        type='video/ogg; codecs="theora, vorbis"' />
-            </video>
-        </div>;
+        let c = this.nowDisplay(auxTrackData, this.props.time)
+        return <div className="jux-aux-display">{c}</div>;
     }
     componentDidMount() {
-        const vid = this.el;
-        vid.currentTime = 5.333;
+        const el = this.el;
+        if (el && el.currentTime) {
+            el.currentTime = 5.333;
+        }
     }
     play() {
         this.el.play();
