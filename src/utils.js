b4bdec1bacdb3aeeaa94a3b31cf3f5f0020cc4bc
Nik Nyby <nnyby@columbia.edu>
2016-09-22 16:23:06 -0400

    Display microseconds - PMT #107922

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
-export function formatDuration(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const newSeconds = pad2(Math.round(seconds - minutes * 60));
-    return minutes + ':' + newSeconds;
+export function formatDuration(totalSeconds) {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = Math.floor(totalSeconds - minutes * 60);
+    const microseconds = Math.round(
+        (totalSeconds - (minutes * 60) - seconds) * 100);
+    return pad2(minutes) + ':' + pad2(seconds) + ':' + pad2(microseconds);
 }
 
 export function pad2(number) {
