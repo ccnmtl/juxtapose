990810aaace51b89568f070f2bcbe395911b52d7
Nik Nyby <nnyby@columbia.edu>
2016-09-30 11:35:20 -0400

    optimize/minimize production build

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ var config = {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
     module: {
         loaders: [
             {
