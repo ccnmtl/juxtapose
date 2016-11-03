3198954f477d43ac1ccc9feb7a12d14f6ad093f0
Nik Nyby <nnyby@columbia.edu>
2016-11-03 16:39:14 -0400

    Enable adding spine video from Mediathread

--- src/SpineVideo.jsx
+++ src/SpineVideo.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+import Vimeo from 'react-vimeo';
 import {createCollectionWidget} from './mediathreadCollection.js';
 
 export default class SpineVideo extends React.Component {
     constructor() {
         super();
         this.id = 'jux-spine-video';
+    }
+    renderMp4Video(url) {
+        return <video id={this.id}
+                      ref={(ref) => this.el = ref}
+                      width="297"
+                      onTimeUpdate={this.onTimeUpdate.bind(this)}
+                      onLoadedMetadata={this.onLoadedMetadata.bind(this)}
+                      onEnded={this.onEnded.bind(this)}>
+            <source src={url} type="video/mp4" />
+        </video>;
+    }
+    renderYoutubeVideo(id) {
+        const opts = {
+            height: 225,
+            width: 300,
+            playerVars: {
+                autoplay: 0,
+                controls: 0
+            }
+        };
+        return <YouTube videoId={id}
+                        opts={opts}
+                        ref={(ref) => this.yel = ref}
+                        onReady={this.props.onYTReady} />;
+    }
+    renderVimeoVideo(id) {
+        return <Vimeo videoId={id}
+                      className={'vimeo ' + this.className}
+                      autoplay={false} />;
     }
     render() {
         if (!this.props.spineVideo) {
@@ -13,20 +44,17 @@ export default class SpineVideo extends React.Component {
               <button onClick={this.onClick}>&#9998;</button>
             </div>;
         }
+        let video = null;
+
+        if (this.props.spineVideo.host === 'youtube') {
+            video = this.renderYoutubeVideo(this.props.spineVideo.url);
+        } else if (this.props.spineVideo.host === 'vimeo') {
+            video = this.renderVimeoVideo(this.props.spineVideo.url);
+        } else {
+            video = this.renderMp4Video(this.props.spineVideo.url);
+        }
         return <div className="jux-spine-display">
-            <video id={this.id}
-                   ref={(ref) => this.el = ref}
-                   width="297"
-                   onTimeUpdate={this.onTimeUpdate.bind(this)}
-                   onLoadedMetadata={this.onLoadedMetadata.bind(this)}
-                   onEnded={this.onEnded.bind(this)}>
-                <source src={this.props.spineVideo.url + '.mp4'}
-                        type="video/mp4" />
-                <source src={this.props.spineVideo.url + '.webm'}
-                        type="video/webm" />
-                <source src={this.props.spineVideo.url + '.ogv'}
-                        type='video/ogg; codecs="theora, vorbis"' />
-            </video>
+            {video}
             <button onClick={this.onClick}>&#9998;</button>
         </div>;
     }
@@ -46,11 +74,20 @@ export default class SpineVideo extends React.Component {
     }
     // TODO: handle playback finish event
     play() {
-        const vid = this.el;
-        vid.play();
+        if (this.el && this.el.paused) {
+            this.el.play();
+        }
+        if (this.yel && this.yel.internalPlayer) {
+            this.yel.internalPlayer.playVideo();
+        }
     }
     pause() {
-        this.el.pause();
+        if (this.el && !this.el.paused) {
+            this.el.pause();
+        }
+        if (this.yel && this.yel.internalPlayer) {
+            this.yel.internalPlayer.pauseVideo();
+        }
     }
     onClick() {
         createCollectionWidget('video');
