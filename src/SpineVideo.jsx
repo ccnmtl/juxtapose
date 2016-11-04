7902cec447dad250dabfae807a2509b64a858c69
Nik Nyby <nnyby@columbia.edu>
2016-11-04 13:38:03 -0400

    Use react-player for spine video
    This was necessary because YouTube's JS api has no built-in ontimeupdate
    callback. This library also simplifies a lot of code, so I'm planning
    to use it for the secondary video display as well.

--- src/SpineVideo.jsx
+++ src/SpineVideo.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
-import YouTube from 'react-youtube';
-import Vimeo from 'react-vimeo';
+import ReactPlayer from 'react-player'
 import {createCollectionWidget} from './mediathreadCollection.js';
 
 export default class SpineVideo extends React.Component {
     constructor() {
         super();
         this.id = 'jux-spine-video';
-    }
-    renderMp4Video(url) {
-        return <video id={this.id}
-                      ref={(ref) => this.el = ref}
-                      width="297"
-                      onTimeUpdate={this.onTimeUpdate.bind(this)}
-                      onLoadedMetadata={this.onLoadedMetadata.bind(this)}
-                      onEnded={this.onEnded.bind(this)}>
-            <source src={url} type="video/mp4" />
-        </video>;
-    }
-    renderYoutubeVideo(id) {
-        const opts = {
-            height: 225,
-            width: 300,
-            playerVars: {
-                autoplay: 0,
-                controls: 0
-            }
-        };
-        return <YouTube videoId={id}
-                        opts={opts}
-                        ref={(ref) => this.yel = ref}
-                        onReady={this.props.onYTReady} />;
-    }
-    renderVimeoVideo(id) {
-        return <Vimeo videoId={id}
-                      className={'vimeo ' + this.className}
-                      autoplay={false} />;
     }
     render() {
         if (!this.props.spineVideo) {
@@ -44,28 +14,39 @@ export default class SpineVideo extends React.Component {
               <button onClick={this.onClick}>&#9998;</button>
             </div>;
         }
-        let video = null;
 
+        let url = null;
         if (this.props.spineVideo.host === 'youtube') {
-            video = this.renderYoutubeVideo(this.props.spineVideo.url);
+            url = 'https://www.youtube.com/watch?v=' +
+                  this.props.spineVideo.url;
         } else if (this.props.spineVideo.host === 'vimeo') {
-            video = this.renderVimeoVideo(this.props.spineVideo.url);
+            url = 'https://www.vimeo.com/watch?v=' + this.props.spineVideo.url;
         } else {
-            video = this.renderMp4Video(this.props.spineVideo.url);
+            url = this.props.spineVideo.url;
         }
+
         return <div className="jux-spine-display">
-            {video}
+                <ReactPlayer
+                    id={this.id}
+                    ref={(ref) => this.player = ref}
+                    width={297}
+                    height={222.25}
+                    playing={this.props.playing}
+                    onLoadedMetadata={this.onLoadedMetadata.bind(this)}
+                    onEnded={this.onEnded.bind(this)}
+                    url={url}
+                    onDuration={this.props.onDuration}
+                    onProgress={this.props.onProgress}
+                    progressFrequency={100}
+                />;
             <button onClick={this.onClick}>&#9998;</button>
         </div>;
     }
-    updateVidPosition(time) {
-        this.el.currentTime = time;
+    updateVidPosition(percent) {
+        console.log(this.player, percent);
+        this.player.seekTo(percent);
     }
     onLoadedMetadata(e) {
-        const vid = e.target;
-        this.props.callbackParent(vid.currentTime, vid.duration);
-    }
-    onTimeUpdate(e) {
         const vid = e.target;
         this.props.callbackParent(vid.currentTime, vid.duration);
     }
@@ -73,22 +54,6 @@ export default class SpineVideo extends React.Component {
         this.props.onVideoEnd();
     }
     // TODO: handle playback finish event
-    play() {
-        if (this.el && this.el.paused) {
-            this.el.play();
-        }
-        if (this.yel && this.yel.internalPlayer) {
-            this.yel.internalPlayer.playVideo();
-        }
-    }
-    pause() {
-        if (this.el && !this.el.paused) {
-            this.el.pause();
-        }
-        if (this.yel && this.yel.internalPlayer) {
-            this.yel.internalPlayer.pauseVideo();
-        }
-    }
     onClick() {
         createCollectionWidget('video');
     }
