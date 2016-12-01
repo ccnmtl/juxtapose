992e8f0fcca67f979da0bb177ec8ddc6fdcf4c61
Nik Nyby <nnyby@columbia.edu>
2016-12-01 13:06:04 -0500

    Use filter by media type when picking spine video

--- src/MediaTrack.jsx
+++ src/MediaTrack.jsx
@@ -8,6 +8,6 @@ export default class MediaTrack extends Track {
         this.type = 'media';
     }
     onAddTrackElementClick() {
-        createCollectionWidget();
+        createCollectionWidget('all');
     }
 }
