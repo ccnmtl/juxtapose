8ab49b593530131897d68a29db8ae2a78ef5746b
Susan Dreher <sdreher@columbia.edu>
2016-12-21 13:12:42 -0500

    Instructions - secondary materials and custom
    Add media display instructions when secondary materials are empty
    
    Pick up custom instructions from the containing view and display

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,10 @@ import JuxtaposeApplication from './JuxtaposeApplication.jsx';
 
 
 ReactDOM.render(
-    <JuxtaposeApplication readOnly={(view && view.submitted) || false} />,
+    <JuxtaposeApplication
+        readOnly={(view && view.submitted) || false}
+        primaryInstructions={view && view.primaryInstructions || ''}
+        secondaryInstructions={view && view.secondaryInstructions || ''}
+    />,
     document.getElementById('jux-container')
 );
