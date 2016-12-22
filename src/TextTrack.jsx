7a9a690d32b7e641688849702996d288656542db
Nik Nyby <nnyby@columbia.edu>
2016-12-22 14:16:37 -0500

    Insert text elements at the correct position

--- src/TextTrack.jsx
+++ src/TextTrack.jsx
@@ -6,11 +6,27 @@ export default class TextTrack extends Track {
     constructor(props) {
         super(props);
         this.type = 'txt';
+        this.state = {
+            adding: false,
+            timecode: 0
+        };
     }
     renderItemAdder() {
         return <TextAnnotationAdder
                    showing={this.state.adding}
                    onCloseClick={this.closeItemAdder.bind(this)}
                    onSubmit={this.onTrackElementAdd.bind(this)} />;
+    }
+    // When the add button is clicked (open modal)
+    onAddTrackElementClick(e, absoluteTimecode) {
+        this.setState({
+            adding: true,
+            timecode: absoluteTimecode
+        });
+    }
+    // When the new element is actually added
+    onTrackElementAdd(value) {
+        this.closeItemAdder();
+        this.props.onTrackElementAdd(value, this.state.timecode);
     }
 }
