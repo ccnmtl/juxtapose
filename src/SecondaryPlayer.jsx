eaa6bd606117d70b80cf0ed26cedf507b762c033
Nik Nyby <nnyby@columbia.edu>
2016-12-29 16:05:48 -0500

    Mute hidden SecondaryPlayers

--- src/SecondaryPlayer.jsx
+++ src/SecondaryPlayer.jsx
@@ -3,9 +3,9 @@ import ReactPlayer from 'react-player';
 
 
 /**
- * Each video/audio element its own SecondaryPlayer instance that's
- * always rendered on the page, and displayed when the sequence's time
- * is at the right position.
+ * Each video/audio element has its own SecondaryPlayer instance
+ * that's always rendered on the page, and displayed when the
+ * sequence's time is at the right position.
  */
 export default class SecondaryPlayer extends React.Component {
     render() {
@@ -18,11 +18,13 @@ export default class SecondaryPlayer extends React.Component {
             url = this.props.data.source;
         }
 
+        const volume = (this.props.hidden) ? 0 : 0.8;
         return <ReactPlayer
             ref={(ref) => this.player = ref}
             width={480}
             height={360}
             url={url}
+            volume={volume}
             hidden={this.props.hidden}
             playing={this.props.playing}
         />;
