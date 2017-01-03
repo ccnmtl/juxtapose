69e1b4f86139528fa0593cd1515a3ac954c8de72
Nik Nyby <nnyby@columbia.edu>
2017-01-03 14:51:35 -0500

    Use normal range input for playhead - PMT #109411

--- src/Playhead.jsx
+++ src/Playhead.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 
 export default class Playhead extends React.Component {
+    constructor(props) {
+        super(props);
+        this.trackWidth = 915;
+    }
     render() {
         if (!this.props.duration) {
             return <div className="jux-playhead-container">
@@ -14,22 +18,21 @@ export default class Playhead extends React.Component {
             currentPos = (this.props.currentTime / this.props.duration);
         }
 
-        const clientWidth = this.el ? this.el.clientWidth : 0;
-        const x = clientWidth * currentPos;
-        const lineStyle = {left: x + 'px'};
+        const x = this.trackWidth * currentPos;
+        const lineStyle = {left: x + 7 + 'px'};
 
         return <div className="jux-playhead-container">
-            <div className="jux-playhead-line"
-                 style={lineStyle}>
-                <div className="jux-playhead-top-cutpoint"></div>
-                <div className="jux-playhead-bottom-cutpoint"></div>
-            </div>
             <input type="range" min="0" max="1000"
                    ref={(ref) => this.el = ref}
                    onChange={this.props.onChange}
                    onMouseDown={this.props.onMouseDown}
                    onMouseUp={this.props.onMouseUp}
                    value={currentPos * 1000} />
+            <div className="jux-playhead-line"
+                 style={lineStyle}>
+                <div className="jux-playhead-top-cutpoint"></div>
+                <div className="jux-playhead-bottom-cutpoint"></div>
+            </div>
         </div>;
     }
 }
