1e3f57868352aeef3289ced889e9ea7569fb64b5
Nik Nyby <nnyby@columbia.edu>
2017-01-04 14:43:34 -0500

    react-bootstrap: only include Modal
    This shrinks my development build from 3.71 MB to 3.25 MB. Not much but
    it's something.

--- src/AddPrimaryMediaModal.jsx
+++ src/AddPrimaryMediaModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Modal} from 'react-bootstrap';
+import Modal from 'react-bootstrap/lib/Modal';
 
 export default class AddPrimaryMediaModal extends React.Component {
     render() {
--- src/TextAnnotationAdder.jsx
+++ src/TextAnnotationAdder.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Modal} from 'react-bootstrap';
+import Modal from 'react-bootstrap/lib/Modal';
 
 export default class TextAnnotationAdder extends React.Component {
     constructor(props) {
