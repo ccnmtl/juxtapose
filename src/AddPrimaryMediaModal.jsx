d13a51026eeb1d8a55184d3273a46d9fdb46a174
Nik Nyby <nnyby@columbia.edu>
2017-01-04 09:55:05 -0500

    Add notice when adding secondary media with no spine - PMT #109422

---
+++ src/AddPrimaryMediaModal.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Modal} from 'react-bootstrap';
+
+export default class AddPrimaryMediaModal extends React.Component {
+    render() {
+        return <Modal show={this.props.showing}
+                      onHide={this.props.onCloseClick}>
+    <Modal.Header closeButton>
+        <Modal.Title>Add Primary Media</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        You must add a primary video before adding secondary elements.
+    </Modal.Body>
+    <Modal.Footer>
+        <button type="button" className="btn btn-primary"
+                onClick={this.props.onCloseClick}>
+            Close
+        </button>
+    </Modal.Footer>
+        </Modal>;
+    }
+}
